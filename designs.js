424351ecd991a4bf37476feb29e975a0e85af785
athousandleaves <superamare@gmail.com>
2018-01-22 08:48:02 -0600

    declare initial vars

---
+++ designs.js
@@ -0,0 +1,5 @@
+const colorPicker = document.getElementById('colorPicker'),
+      sizePicker  = document.getElementById('sizePicker'),
+      colorDiv    = document.getElementById('colorDiv'),
+      canvas      = document.getElementById('pixel_canvas'),
+      reset       = document.createElement('button');
