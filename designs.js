dfba170980951297f5f26e6d06006962c0a2d2aa
athousandleaves <superamare@gmail.com>
2018-01-22 08:50:19 -0600

    add function to create canvas grid

--- designs.js
+++ designs.js
@@ -1,5 +1,27 @@
-const colorPicker = document.getElementById('colorPicker'),
-      sizePicker  = document.getElementById('sizePicker'),
-      colorDiv    = document.getElementById('colorDiv'),
-      canvas      = document.getElementById('pixel_canvas'),
-      reset       = document.createElement('button');
+const colorPicker = document.getElementById("colorPicker"),
+      sizePicker  = document.getElementById("sizePicker"),
+      colorDiv    = document.getElementById("colorDiv"),
+      canvas      = document.getElementById("pixel_canvas"),
+      reset       = document.createElement("button");
+
+const makeGrid = () => {
+  // display color picker
+  colorDiv.style.display = "grid";
+  // set up grid
+  let height = document.getElementById("input_height").value;
+  let width = document.getElementById("input_width").value;
+  // create rows
+  for (let x = 0; x < height; x++) {
+    let tr = document.createElement("tr");
+    canvas.appendChild(tr);
+    // create columns
+    for (let y = 0; y < width; y++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+    }
+  }
+  // place reset button below grid
+  reset.className = "reset";
+  reset.textContent = "Reset";
+  document.body.appendChild(reset);
+};
